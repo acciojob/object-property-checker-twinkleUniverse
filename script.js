b9a01f24f0b35d732cc37e5edc87e8d1016a4977
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-21 18:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-    console.log(sampleObject.hasOwnProperty(key))
+    return sampleObject.hasOwnProperty(key)
 }
 
 // Do not change the code below
